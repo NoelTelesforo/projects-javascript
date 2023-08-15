e7c238010c5a09f05c79cfb04429585d1ec575c3
Noel Telesforo <ntelesforomartinez@gmail.com>
2023-08-14 19:40:46 -0600

    Función que registra los EventListeners

--- js/app.js
+++ js/app.js
@@ -4,3 +4,16 @@ const listaCursos = document.querySelector('#lista-cursos');
 const contenedorCarrito = document.querySelector('#lista-carrito tbody');
 const vaciarCarritoBtn =document.querySelector('vaciar-carrito');
 let articulosCarito = [];
+
+
+// Listeners
+cargarEventListeners();
+
+function cargarEventListeners() {
+    // Agregar curso al carrito
+    listaCursos.addEventListener('click', agregarCurso);
+    // Eliminar curso del carrito
+    carrito.addEventListener('click', eliminarCurso);
+    // Vaciar el carrito
+    vaciarCarritoBtn.addEventListener('click', vaciarCarrito);
+}
