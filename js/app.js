42498dfdfdd0fe0efdae04eb0b1a504a413d7620
Noel Telesforo <ntelesforomartinez@gmail.com>
2023-08-14 20:02:53 -0600

    Muestra el curso seleccionado en el carrito

--- js/app.js
+++ js/app.js
@@ -62,3 +62,23 @@ function eliminarCurso(e) {
         carritoHTML();
     }
 }
+
+function carritoHTML() {
+    vaciarCarrito();
+
+    articulosCarrito.forEach(curso => {
+        const row = document.createElement('tr');
+        row.innerHTML = `
+            <td>
+                <img src="${curso.imagen}" width=100>
+            </td>
+            <td>${curso.titulo}</td>
+            <td>${curso.precio}</td>
+            <td>${curso.cantidad}</td>
+            <td>
+                <a href="#" class="borrar-curso" data-id="${curso.id}">X</a>
+            </td>
+        `;
+        contenedorCarrito.appendChild(row);
+    });
+}
