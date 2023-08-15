f1ed8c65f98dfbc9853908d72962e66e91996d49
Noel Telesforo <ntelesforomartinez@gmail.com>
2023-08-14 19:34:48 -0600

    Se declaran variables

---
+++ js/app.js
@@ -0,0 +1,6 @@
+// Variables
+const carrito = document.querySelector('#carrito');
+const listaCursos = document.querySelector('#lista-cursos');
+const contenedorCarrito = document.querySelector('#lista-carrito tbody');
+const vaciarCarritoBtn =document.querySelector('vaciar-carrito');
+let articulosCarito = [];
